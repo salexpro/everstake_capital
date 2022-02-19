63b1c0fb8568ec8990bb66574272cdea940be736
Alex <dev@salex.pro>
2022-02-19 04:43:05 +0200

    feat: partners block animation

--- src/components/Partners/index.js
+++ src/components/Partners/index.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container } from 'react-bootstrap'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
 import logos from './img/logos.svg'
 import logosMd from './img/logos_md.svg'
@@ -8,8 +10,27 @@ import logosSm from './img/logos_sm.svg'
 import * as s from './Partners.module.scss'
 
 const Partners = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      ScrollTrigger.create({
+        trigger: '#partners',
+        start: '20% center',
+        end: '20% center',
+        // markers: true,
+        onEnter: () => {
+          gsap.to(`.${s.partners__img}`, {
+            autoAlpha: 1,
+            x: 0,
+            duration: 2,
+            ease: 'expo.out',
+          })
+        },
+      })
+    }, 1000)
+  }, [])
+
   return (
-    <Container as="section" className={s.partners}>
+    <Container as="section" id="partners" className={s.partners}>
       <h2 className={s.partners__header}>Where are we in these innovations?</h2>
       <p className="lead">
         We&apos;ve invested in 25 crypto startups and launched 50 validator
